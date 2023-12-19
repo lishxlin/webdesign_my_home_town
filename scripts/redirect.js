81ddf868df36c715cbb5b85fa3746ff14617709a
LiShXlin <lishxlin@outlook.com>
2023-12-20 00:52:46 +0800

    ADD MORE AND MORE!

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,3 +1,7 @@
 document.getElementById('bridge').addEventListener('click', function() {
   window.location.href = "/wuhan_bridge.html"; // Replace with your actual URL
 });
+
+document.getElementById('railway').addEventListener('click', function() {
+  window.location.href = "/wuhan_railway.html"; // Replace with your actual URL
+});
