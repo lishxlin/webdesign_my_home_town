7935ccdad9b170e59dbc7e8210fbddff92739859
LiShXlin <lishxlin@outlook.com>
2023-12-20 17:03:20 +0800

    Login site

--- scripts/login-site.js
+++ scripts/login-site.js
@@ -1,9 +1,47 @@
 function check()
 {
+	let user = "admin";
+	let passwd = "admin";
+	let getUsername = document.getElementById("usr").value;
+	console.log(getUsername);
+	let getPasscode = document.getElementById("pwd").value;
+	console.log(getPasscode);
+	if (getUsername == user && getPasscode == passwd)
+	{
+		console.log("hi");
+		document.getElementById("form").remove();
 
+		let newnode = document.createElement("p");
+		let newtext = document.createTextNode("Login Successful, redirecting you to control console......");
+		newnode.appendChild(newtext);
+
+		document.getElementById("global").append(newnode);
+	}
+	else
+	{
+		alert("Username or Password is not correct, please try again!");
+		document.getElementById("usr").value = "";
+		document.getElementById("pwd").value = "";
+	}
+}
+
+function reset()
+{
+	alert("Page Resetting...");
 }
 
 function back()
 {
 	window.location.href = "/Changjiang.html"; 
 }
+
+function check_root()
+{
+	if(document.getElementById("usr").value == "root")
+	{
+		document.getElementById("usr").value = "";
+		alert("Username should not be root!");
+	}
+}
+
+setInterval(check_root, 50);
